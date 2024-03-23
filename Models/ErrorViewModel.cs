e1b842d00f82e42d4bd455ece19ec967c3a80e61
Leo-Zubiri <leo.zubiri@outlook.com>
2024-03-22 22:46:53 -0600

    first commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace PortalAspnetCore.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
